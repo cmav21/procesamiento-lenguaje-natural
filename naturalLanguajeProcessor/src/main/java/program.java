ecc4b2115d7a4d44a115c7b23676ec952d5c991b
cesar magallon <cesar9mv@gmail.com>
2018-11-28 22:25:39 -0600

    [add] count syllables and characters

--- naturalLanguajeProcessor/src/main/java/program.java
+++ naturalLanguajeProcessor/src/main/java/program.java
@@ -5,6 +5,42 @@ import java.util.Hashtable;
 
 public class program {
 
+    public static int contarSilabas(String word){
+        int count = 0;
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == '\"' || word.charAt(i) == '\'' || word.charAt(i) == '-' || word.charAt(i) == ',' || word.charAt(i) == ')' || word.charAt(i) == '(') {
+                word = word.substring(0,i)+word.substring(i+1, word.length());
+            }
+        }
+        boolean isPrevVowel = false;
+        for (int j = 0; j < word.length(); j++) {
+            if (word.contains("a") || word.contains("e") || word.contains("i") || word.contains("o") || word.contains("u")) {
+                if (esVocal(word.charAt(j)) && !((word.charAt(j) == 'e') && (j == word.length()-1))) {
+                    if (isPrevVowel == false) {
+                        count++;
+                        isPrevVowel = true;
+                    }
+                } else {
+                    isPrevVowel = false;
+                }
+            } else {
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static boolean esVocal(char c){
+
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
     public static void addContenidoToHash(Hashtable repetidas, Hashtable hash, String[] array)  {
 
@@ -27,20 +63,24 @@ public class program {
     }
 
 
-    public static void WriteTextInfile(PrintWriter print_line, String fileName, Hashtable repetidas, Hashtable sinRepetir, boolean cerrar) throws IOException{
+    public static void WriteTextInfile(PrintWriter print_line, String fileName, Hashtable repetidas, Hashtable sinRepetir, boolean cerrar,int caracteres,int silabas) throws IOException{
 
-        print_line.println("palabras sin repetir en " + fileName + ":");
+        print_line.println("cantidad de palabras repetidas en " + fileName + ":");
 
         Enumeration<String> enumeration = repetidas.elements();
-        while (enumeration.hasMoreElements()) {
-            print_line.println(enumeration.nextElement());
-        }
+        print_line.println(repetidas.size());
 
-        print_line.println("palabras repetidas en " + fileName + ":");
-        Enumeration<String> words = sinRepetir.elements();
-        while (words.hasMoreElements()) {
-            print_line.println(words.nextElement());
-        }
+
+        print_line.println("cantidad de palabras sin repetir en " + fileName + ":");
+        print_line.println(sinRepetir.size());
+
+        print_line.println("cantidad de caracteres en " + fileName + ":");
+        print_line.println(caracteres);
+
+        print_line.println("cantidad de silabas en " + fileName + ":");
+        print_line.println(silabas);
+
+
 
         print_line.println("\n");
 
@@ -54,9 +94,11 @@ public class program {
 
     }
 
+
     public static void main(String[] args) throws FileNotFoundException, java.io.IOException {
         String contenido = "",total="";
         String[] saltos,array;
+        int totalCharacterCount = 0,totalSilabas = 0;
 
         Hashtable<String,String> sinRepetir = new Hashtable<String, String>();
         Hashtable<String,String> repetidas = new Hashtable<String, String>();
@@ -65,17 +107,25 @@ public class program {
         PrintWriter print_line = new PrintWriter(write);
 
         for(int i = 0; i<3;i++) {
+
             FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
             dialog.setMode(FileDialog.LOAD);
             dialog.setVisible(true);
             String file = dialog.getDirectory();
             String name = dialog.getFile();
+            int characterCount = 0,silabas = 0;
 
             BufferedReader in = new BufferedReader(new FileReader(file + name));
             String line = "", text="";
             while ((line = in.readLine()) != null) {
                 text += line + " ";
                 total += line + " ";
+                silabas += contarSilabas(line);
+                totalSilabas += contarSilabas(line);
+                if(!(line.equals(""))){
+                    characterCount += line.length();
+                    totalCharacterCount += line.length();
+                }
             }
             in.close();
 
@@ -88,7 +138,8 @@ public class program {
 
             addContenidoToHash(sinRepetir, repetidas,array);
 
-            WriteTextInfile(print_line,name,repetidas,sinRepetir,false);
+
+            WriteTextInfile(print_line,name,repetidas,sinRepetir,false,characterCount,silabas);
 
 
         }
@@ -96,7 +147,7 @@ public class program {
         String[] prueba = total.split(" ");
         addContenidoToHash(sinRepetir, repetidas,prueba);
 
-        WriteTextInfile(print_line,"en todos los archivos",repetidas,sinRepetir,true);
+        WriteTextInfile(print_line,"en todos los archivos",repetidas,sinRepetir,true,totalCharacterCount,totalSilabas);
 
     }
 
