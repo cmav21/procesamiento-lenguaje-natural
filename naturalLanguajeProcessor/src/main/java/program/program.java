af99ab3bb162885fa99e16ce79bcdefa4902b9f3
cesar magallon <cesar9mv@gmail.com>
2018-11-28 22:41:59 -0600

    changes

---
+++ naturalLanguajeProcessor/src/main/java/program/program.java
@@ -0,0 +1,67 @@
+package program;
+
+import java.awt.*;
+import java.io.*;
+import java.util.Hashtable;
+import static program.usaClase.*;
+
+public class program {
+
+
+    public static void main(String[] args) throws FileNotFoundException, java.io.IOException {
+        String contenido = "",total="";
+        String[] saltos,array;
+        int totalCharacterCount = 0,totalSilabas = 0;
+
+        Hashtable<String,String> sinRepetir = new Hashtable<String, String>();
+        Hashtable<String,String> repetidas = new Hashtable<String, String>();
+
+        FileWriter write = new FileWriter("/home/cesar/new.txt",false);
+        PrintWriter print_line = new PrintWriter(write);
+
+        for(int i = 0; i<3;i++) {
+
+            FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
+            dialog.setMode(FileDialog.LOAD);
+            dialog.setVisible(true);
+            String file = dialog.getDirectory();
+            String name = dialog.getFile();
+            int characterCount = 0,silabas = 0;
+
+            BufferedReader in = new BufferedReader(new FileReader(file + name));
+            String line = "", text="";
+            while ((line = in.readLine()) != null) {
+                text += line + " ";
+                total += line + " ";
+                silabas +=  contarSilabas(line);
+                totalSilabas += contarSilabas(line);
+                if(!(line.equals(""))){
+                    characterCount += line.length();
+                    totalCharacterCount += line.length();
+                }
+            }
+            in.close();
+
+            saltos = text.split("\t");
+            for(int j = 0; j<saltos.length;j++) {
+                contenido = saltos[j] + " ";
+            }
+
+            array = contenido.split(" ");
+
+            addContenidoToHash(sinRepetir, repetidas,array);
+
+
+            WriteTextInfile(print_line,name,repetidas,sinRepetir,false,characterCount,silabas);
+
+
+        }
+
+        String[] prueba = total.split(" ");
+        addContenidoToHash(sinRepetir, repetidas,prueba);
+
+        WriteTextInfile(print_line,"en todos los archivos",repetidas,sinRepetir,true,totalCharacterCount,totalSilabas);
+
+    }
+
+}
--- naturalLanguajeProcessor/src/main/java/program/usaClase.java
+++ naturalLanguajeProcessor/src/main/java/program/usaClase.java
@@ -1,10 +1,11 @@
-import java.awt.*;
-import java.io.*;
+package program;
+
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
-public class program {
-
+public class usaClase {
     public static int contarSilabas(String word){
         int count = 0;
         word = word.toLowerCase();
@@ -63,7 +64,7 @@ public class program {
     }
 
 
-    public static void WriteTextInfile(PrintWriter print_line, String fileName, Hashtable repetidas, Hashtable sinRepetir, boolean cerrar,int caracteres,int silabas) throws IOException{
+    public static void WriteTextInfile(PrintWriter print_line, String fileName, Hashtable repetidas, Hashtable sinRepetir, boolean cerrar, int caracteres, int silabas) throws IOException {
 
         print_line.println("cantidad de palabras repetidas en " + fileName + ":");
 
@@ -93,62 +94,4 @@ public class program {
         }
 
     }
-
-
-    public static void main(String[] args) throws FileNotFoundException, java.io.IOException {
-        String contenido = "",total="";
-        String[] saltos,array;
-        int totalCharacterCount = 0,totalSilabas = 0;
-
-        Hashtable<String,String> sinRepetir = new Hashtable<String, String>();
-        Hashtable<String,String> repetidas = new Hashtable<String, String>();
-
-        FileWriter write = new FileWriter("/home/cesar/new.txt",false);
-        PrintWriter print_line = new PrintWriter(write);
-
-        for(int i = 0; i<3;i++) {
-
-            FileDialog dialog = new FileDialog((Frame) null, "Select File to Open");
-            dialog.setMode(FileDialog.LOAD);
-            dialog.setVisible(true);
-            String file = dialog.getDirectory();
-            String name = dialog.getFile();
-            int characterCount = 0,silabas = 0;
-
-            BufferedReader in = new BufferedReader(new FileReader(file + name));
-            String line = "", text="";
-            while ((line = in.readLine()) != null) {
-                text += line + " ";
-                total += line + " ";
-                silabas += contarSilabas(line);
-                totalSilabas += contarSilabas(line);
-                if(!(line.equals(""))){
-                    characterCount += line.length();
-                    totalCharacterCount += line.length();
-                }
-            }
-            in.close();
-
-            saltos = text.split("\t");
-            for(int j = 0; j<saltos.length;j++) {
-                contenido = saltos[j] + " ";
-            }
-
-            array = contenido.split(" ");
-
-            addContenidoToHash(sinRepetir, repetidas,array);
-
-
-            WriteTextInfile(print_line,name,repetidas,sinRepetir,false,characterCount,silabas);
-
-
-        }
-
-        String[] prueba = total.split(" ");
-        addContenidoToHash(sinRepetir, repetidas,prueba);
-
-        WriteTextInfile(print_line,"en todos los archivos",repetidas,sinRepetir,true,totalCharacterCount,totalSilabas);
-
-    }
-
 }
